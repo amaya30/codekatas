94a4bae80110bd0ff9d40d10959424cae065bf00
Rebecca Long <rebecca.long@gmail.com>
2015-07-22 19:20:56 -0700

    FizzBuzz Kata

---
+++ katas/fizzbuzz/FizzBuzz/FizzBuzzKata.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace FizzBuzz
+{
+    public class FizzBuzzKata
+    {
+        public static void Main(string[] args)
+        {
+            FizzBuzzKata fizzBuzz = new FizzBuzzKata();
+        }
+
+        public FizzBuzzKata()
+        {
+            Console.WriteLine("Starting Number: ");
+            var startNum = GetNumericInput();
+            Console.WriteLine("Ending Number: ");
+            var endNum = GetNumericInput();
+
+            Console.WriteLine("\r\nFizzBuzz Sequence: ");
+            for (var i = startNum; i <= endNum; i++)
+            {
+                Console.WriteLine(Answer(i));
+            }
+        }
+
+        private int GetNumericInput()
+        {
+            string input = null;
+            while (input == null)
+            {
+                Console.WriteLine("Enter numeric value...");
+                input = Console.ReadLine();
+            }
+
+            return Int32.Parse(input);
+        }
+
+        private string Answer(int num)
+        {
+            if (num%3 == 0 && num%5 == 0) return "fizzbuzz";
+            if (num%3 == 0) return "fizz";
+            if (num%5 == 0) return "buzz";
+
+            return num.ToString();
+        }
+    }
+}
