770f9c90f18864a7216ff16661558d4086d220ca
Rebecca Long <rebecca.long@gmail.com>
2015-07-22 21:44:35 -0700

    Initial TeaPartyKata commit

---
+++ katas/teaparty/TeaPartyKata/TeaPartyKata.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeaPartyKata
+{
+    class TeaPartyKata
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ katas/teaparty/TeaPartyKata/TeaPartyTest/TeaPartyTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TeaPartyTest
+{
+    [TestClass]
+    public class TeaPartyTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
