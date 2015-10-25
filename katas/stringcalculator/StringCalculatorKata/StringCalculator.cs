b8778d54054545ad6ce61b3b0c4c1dc5557d624a
Rebecca Long <rebecca.long@gmail.com>
2015-10-24 20:57:04 -0700

    Start of string calculator & tea party katas

---
+++ katas/stringcalculator/StringCalculatorKata/StringCalculator.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+
+namespace StringCalculatorKata
+{
+    public class StringCalculator
+    {
+        public int Add(string numbers)
+        {
+            return IsEmpty(numbers) ? 0 : GetSum(numbers);
+        }
+
+        private bool IsEmpty(string numbers)
+        {
+            return numbers.Length < 1;
+        }
+
+        private int GetSum(string numbers)
+        {
+            var delimiter = GetPossibleDelimiters();
+            if (ContainsAny(numbers, delimiter))
+                return numbers.Split(delimiter.ToCharArray()).Sum(n => Convert.ToInt32(n));
+
+            return Convert.ToInt32(numbers);
+        }
+
+        private string GetPossibleDelimiters()
+        {
+            return ",\n";
+        }
+
+        private bool ContainsAny(string numbers, string delimiter)
+        {
+            return delimiter.ToCharArray().Any(numbers.Contains);
+        }
+    }
+}
---
+++ katas/stringcalculator/StringCalculatorKata/StringCalculatorTests.cs
@@ -0,0 +1,48 @@
+﻿using System.Threading;
+
+namespace StringCalculatorKata
+{
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class StringCalculatorTests
+    {
+        [Test]
+        public void Sum_EmptyString_ReturnsZero()
+        {
+            var stringCalc = new StringCalculator();
+            Assert.AreEqual(0, stringCalc.Add(""));
+        }
+
+        [TestCase("1", 1)]
+        [TestCase("2", 2)]
+        public void Sum_SingleNumber_ReturnsThatNumber(string numbers, int expected)
+        {
+            var stringCalc = new StringCalculator();
+            Assert.AreEqual(expected, stringCalc.Add(numbers));
+        }
+
+        [TestCase("1,2", 3)]
+        [TestCase("3,4", 7)]
+        public void Sum_MultipleNumbers_ReturnsSumOfNumbers(string numbers, int expected)
+        {
+            var stringCalc = new StringCalculator();
+            Assert.AreEqual(expected, stringCalc.Add(numbers));
+        }
+
+        [TestCase("1,2,3", 6)]
+        [TestCase("1,2,3,4", 10)]
+        public void Sum_UnknownNumberOfNumbers_ReturnsSumOfNumbers(string numbers, int expected)
+        {
+            var stringCalc = new StringCalculator();
+            Assert.AreEqual(expected, stringCalc.Add(numbers));
+        }
+
+        [Test]
+        public void Sum_NewLineDelimiter_ReturnsSumOfNumbers()
+        {
+            var stringCalc = new StringCalculator();
+            Assert.AreEqual(3, stringCalc.Add("1\n2"));
+        }
+    }
+}
